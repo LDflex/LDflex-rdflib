55e47bd90cc29a790388cb09c6dda97d62496e53
Ruben Verborgh <ruben@verborgh.org>
2020-02-28 23:05:45 +0100

    Test empty sources case.

--- src/RdflibQueryEngine.js
+++ src/RdflibQueryEngine.js
@@ -1,19 +1,51 @@
+import {
+  graph,
+  SPARQLToQuery as sparqlToQuery,
+} from 'rdflib';
+
 /**
  * Asynchronous iterator wrapper for the rdflib.js SPARQL query engine.
  */
 export default class RdflibQueryEngine {
   /**
+   * Creates a query engine with the given sources as default.
+   */
+  constructor(defaultSources) {
+    this._defaultStore = this.readSources(defaultSources);
+  }
+
+  /**
    * Creates an asynchronous iterable of results for the given SPARQL query.
    */
-  async* execute(sparql, source) {
+  async* execute(sparql, sources) {
     if ((/^\s*(?:INSERT|UPDATE)/i).test(sparql))
-      yield* this.executeUpdate(sparql, source);
+      yield* this.executeUpdate(sparql, sources);
+
+    // Parse the SPARQL query
+    const query = sparqlToQuery(sparql, true, graph());
+    // Load the sources if passed, the default sources otherwise
+    const store = await (sources ? this.readSources(sources) : this._defaultStore);
+
+    // Execute the query and store the results in an array
+    const results = [];
+    await new Promise((resolve, reject) => {
+      store.query(query, result => results.push(result), null,
+        error => error ? reject(error) : resolve(results));
+    });
+    yield* results;
   }
 
   /**
    * Creates an asynchronous iterable with the results of the SPARQL UPDATE query.
    */
-  async* executeUpdate(sparql, source) {
+  async* executeUpdate(sparql, sources) {
     throw new Error(`SPARQL UPDATE queries are unsupported, received: ${sparql}`);
+  }
+
+  /**
+   * Reads the specified stores into a store.
+   */
+  async readSources(sources, store = graph()) {
+    return store;
   }
 }
--- test/RdflibQueryEngine-test.js
+++ test/RdflibQueryEngine-test.js
@@ -1,21 +1,61 @@
 import RdflibQueryEngine from '../src/RdflibQueryEngine';
 
-describe('An RdflibQueryEngine instance', () => {
+import { graph } from 'rdflib';
+
+jest.mock('rdflib', () => {
+  const rdflib = jest.requireActual('rdflib');
+  rdflib.graph = jest.fn(rdflib.graph);
+  return rdflib;
+});
+
+const SELECT_TYPES = `
+  SELECT ?subject ?type WHERE {
+    ?subject a ?type.
+  }
+`;
+
+describe('An RdflibQueryEngine instance without default source', () => {
   const engine = new RdflibQueryEngine();
 
+  it('yields no results for a SELECT query', async () => {
+    const result = engine.execute(SELECT_TYPES);
+    expect(await readAll(result)).toHaveLength(0);
+  });
+
+  it('yields no results for a SELECT query with an empty source', async () => {
+    const result = engine.execute(SELECT_TYPES, []);
+    expect(await readAll(result)).toHaveLength(0);
+  });
+
+  it('throws an error when query execution fails', async () => {
+    const store = graph();
+    store.query = (query, onResult, fetcher, onEnd) =>
+      onEnd(new Error('query failure'));
+    graph
+      .mockReturnValueOnce(store)
+      .mockReturnValueOnce(store);
+
+    const result = engine.execute(SELECT_TYPES, []);
+    await expect(readAll(result)).rejects
+      .toThrow('query failure');
+  });
+
   it('does not support UPDATE queries', async () => {
-    const results = engine.execute('UPDATE xyz');
-    await expect((async () => {
-      for await (const result of results)
-        throw new Error('unexpected result');
-    })()).rejects.toThrow('SPARQL UPDATE queries are unsupported, received: UPDATE xyz');
+    const result = engine.execute('UPDATE xyz');
+    await expect(readAll(result)).rejects
+      .toThrow('SPARQL UPDATE queries are unsupported, received: UPDATE xyz');
   });
 
   it('does not support INSERT queries', async () => {
-    const results = engine.execute(' insert xyz');
-    await expect((async () => {
-      for await (const result of results)
-        throw new Error('unexpected result');
-    })()).rejects.toThrow('SPARQL UPDATE queries are unsupported, received:  insert xyz');
+    const result = engine.execute(' insert xyz');
+    await expect(readAll(result)).rejects
+      .toThrow('SPARQL UPDATE queries are unsupported, received:  insert xyz');
   });
 });
+
+async function readAll(asyncIterator) {
+  const items = [];
+  for await (const item of asyncIterator)
+    items.push(item);
+  return items;
+}
